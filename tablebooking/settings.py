c9e0f24e920b244fc04e0843bc051ca790105bc6
simonmortensen23 <simon020393@gmail.com>
2022-07-19 13:16:25 +0000

    Fix Allowed Host link

--- tablebooking/settings.py
+++ tablebooking/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['table-bookin22.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['table-booking2022.herokuapp.com', 'localhost']
 
 
 # Application definition
