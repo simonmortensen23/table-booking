660cd097a07f1f1c71085ded6f96769ad545cbd2
simonmortensen23 <simon020393@gmail.com>
2022-07-20 14:24:20 +0000

    Setup account functions

--- feedme/urls.py
+++ feedme/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from feedme.views import RestaurantView
 
 urlpatterns = [
-    path('restaurants/', RestaurantView.as_view(), name='home')
+    path('', RestaurantView.as_view(), name='home')
 ]
--- feedme/views.py
+++ feedme/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from .models import Restaurant
 # Create your views here.
 
 class RestaurantView(generic.ListView):
     model = Restaurant
+    queryset = Restaurant.objects.all()
+    template_name = "index.html"
