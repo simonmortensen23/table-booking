4097884094d815e2f4b60e4d38d8d9e4dcf18988
simonmortensen23 <simon020393@gmail.com>
2022-07-28 15:25:12 +0000

    Fixing bootstrap and responsiveness for templates

--- tablebooking/settings.py
+++ tablebooking/settings.py
@@ -107,16 +107,16 @@ WSGI_APPLICATION = 'tablebooking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 ACCOUNT_EMAIL_REQUIRED = True
@@ -156,6 +156,7 @@ USE_TZ = True
 
 
 #Time Formats
+
 TIME_INPUT_FORMATS = [
     '%H:%M:%S',     # '14:30:59'
     '%H:%M:%S.%f',  # '14:30:59.000200'
