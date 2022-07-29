08c5cecb0b914c551f0b838ba13c104d0aa55d38
simonmortensen23 <simon020393@gmail.com>
2022-07-29 16:21:45 +0000

    Changed order of imports in settings.py

--- tablebooking/settings.py
+++ tablebooking/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
 from django.contrib.messages import constants as messages
 import dj_database_url
 
