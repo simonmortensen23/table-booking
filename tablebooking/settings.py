3b4437bba7d2c5d00aab84646890cebc517f7052
simonmortensen23 <simon020393@gmail.com>
2022-07-29 15:48:53 +0000

    Change debug to False

--- tablebooking/settings.py
+++ tablebooking/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['table-booking2022.herokuapp.com', 'localhost']
 
