c356faadc9abae402708be712ef25504290fb5ac
simonmortensen23 <simon020393@gmail.com>
2022-07-30 10:01:12 +0000

    Final commit

--- feedme/views.py
+++ feedme/views.py
@@ -1,10 +1,13 @@
+'''
+Views for application
+'''
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import Booking
 from .forms import BookingForm
 
-# Create your views here.
+
 def status_500(request, *args, **kwargs):
     bookings = Booking.objects.all()
     context = {
@@ -13,6 +16,7 @@ def status_500(request, *args, **kwargs):
     response = render(request, '500.html', context)
     response.status_code = 500
     return response
+
 
 def view_home(request):
     """
@@ -36,8 +40,6 @@ def make_booking(request):
     """
     if request.method == 'POST':
         form = BookingForm(request.POST)
-        # validation_response = validate_booking(form)
-        # if not validation_response:
         if form.is_valid():
             booking = form.save()
             booking.user = request.user
@@ -51,23 +53,7 @@ def make_booking(request):
         'form': form
         }
     return render(request, 'make_booking.html', context)
-    
-    
-# def validate_booking(booking_form):
-#     form = BookingForm(booking_form)
-#     if form.is_valid():
-#         return 'Booking date must be in the future.'
-#     elif find_booking(form):
-#         return 'booking already exists'
 
-# def find_booking(form):
-#     user = request.user
-#     booking_date = form.booking_date
-#     booking_time = form.booking_time
-#     unique_booking = Booking.objects.filter(
-#         user=user, booking_date=booking_date, booking_time=booking_time).exists()
-#     return unique_booking
-        
 
 @login_required
 def view_booking(request):
@@ -79,7 +65,8 @@ def view_booking(request):
     context = {
         'bookings': bookings
     }
-    return render(request, 'view_booking.html', context)
+    return render(
+        request, 'view_booking.html', context)
 
 
 @login_required
@@ -116,6 +103,3 @@ def delete_booking(request, booking_id):
     booking.delete()
     messages.success(request, 'Your booking has been deleted.')
     return redirect('view_booking')
-
-
-
